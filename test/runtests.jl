622fe3c73b68e6049cac2def420c54f8c3ab2fab
Chris Elrod <elrodc@gmail.com>
2021-08-15 11:54:21 -0400

    add primitive test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,7 @@ using Test
 
   @test @inferred(CPUSummary.sys_threads()) == Sys.CPU_THREADS::Int
   @test @inferred(CPUSummary.num_threads()) == Threads.nthreads()
-  
+  for i ∈ 1:4
+    @test CPUSummary.redefine_cache(1) === nothing
+  end
 end
