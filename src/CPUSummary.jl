3b1f07f3b4e31f73201bf5e794977196b4cf7a85
Chris Elrod <elrodc@gmail.com>
2022-03-01 19:08:11 -0500

    drop hwloc

--- src/CPUSummary.jl
+++ src/CPUSummary.jl
@@ -1,25 +1,11 @@
 module CPUSummary
 
-using Static, Hwloc
+using Static
 using Static: Zero, One, gt, lt
 using IfElse: ifelse
-
 export cache_size, cache_linesize, cache_associativity, cache_type,
   cache_inclusive, num_cache, num_cores, num_threads
 
-include("topology.jl")
-const BASELINE_CORES = Int(num_cores()) * ((Sys.ARCH === :aarch64) && Sys.isapple() ? 2 : 1)
-function __init__()
-  Sys.isapple() && Sys.ARCH === :aarch64 && return # detect M1
-  ccall(:jl_generating_output, Cint, ()) == 1 && return
-  safe_topology_load!()
-  if count_attr(:Core) ≢ BASELINE_CORES
-    redefine_attr_count()
-    foreach(redefine_cache, 1:4)
-  end
-  redefine_num_threads()
-  return nothing
-end
-
+include("generic_topology.jl")
 
 end
---
+++ src/generic_topology.jl
@@ -0,0 +1,72 @@
+
+redefine_attr_count() = nothing
+num_machines() = static(1)
+num_sockets() = static(1)
+
+function _redefine()
+  nc = (Sys.CPU_THREADS)::Int>>1
+  syst = Sys.CPU_THREADS::Int
+  nt = Threads.nthreads()
+  if nc != num_l1cache()
+    @eval num_l1cache() = static($nc)
+  end
+  if nc != num_cores()
+    @eval num_cores() = static($nc)
+  end
+  if syst != sys_threads()
+    @eval sys_threads() = static($syst)
+  end
+  if nt != num_threads()
+    @eval num_threads() = static($nt)
+  end
+end
+
+let nc = static((Sys.CPU_THREADS)::Int>>1)
+  global num_l1cache() = nc
+  global num_cores() = nc
+end
+let syst = static((Sys.CPU_THREADS)::Int)
+  global sys_threads() = syst
+  global num_threads() = syst
+end
+
+num_l2cache() = num_l1cache()
+num_l3cache() = static(1)
+num_l4cache() = static(0)
+
+if Sys.CPU_NAME === "tigerlake"
+  cache_size(::Union{Val{1},StaticInt{1}}) = StaticInt{49152}()
+else
+  cache_size(::Union{Val{1},StaticInt{1}}) = StaticInt{32768}()
+end
+cache_linesize(::Union{Val{1},StaticInt{1}}) = StaticInt{64}()
+cache_associativity(::Union{Val{1},StaticInt{1}}) = StaticInt{0}()
+cache_type(::Union{Val{1},StaticInt{1}}) = Val{:Data}()
+cache_inclusive(::Union{Val{1},StaticInt{1}}) = False()
+
+if Sys.CPU_NAME === "skylake-avx512" || Sys.CPU_NAME === "cascadelake"
+  cache_size(::Union{Val{2},StaticInt{2}}) = StaticInt{1048576}()
+elseif Sys.CPU_NAME === "tigerlake"
+  cache_size(::Union{Val{2},StaticInt{2}}) = StaticInt{1310720}()
+elseif occursin("zn", Sys.CPU_NAME) || occursin("icelake", Sys.CPU_NAME)
+  cache_size(::Union{Val{2},StaticInt{2}}) = StaticInt{524288}()
+else
+  cache_size(::Union{Val{2},StaticInt{2}}) = StaticInt{262144}()
+end
+cache_linesize(::Union{Val{2},StaticInt{2}}) = StaticInt{64}()
+cache_associativity(::Union{Val{2},StaticInt{2}}) = StaticInt{0}()
+cache_type(::Union{Val{2},StaticInt{2}}) = Val{:Unified}()
+cache_inclusive(_) = False()
+cache_linesize(::Union{Val{3},StaticInt{3}}) = StaticInt{64}()
+
+cache_type(::Union{Val{3},StaticInt{3}}) = Val{:Unified}()
+cache_size(::Union{Val{3},StaticInt{3}}) = num_cores() * StaticInt{1441792}()
+
+num_cache(::Union{Val{1},StaticInt{1}}) = num_l1cache()
+num_cache(::Union{Val{2},StaticInt{2}}) = num_l2cache()
+num_cache(::Union{Val{3},StaticInt{3}}) = num_l3cache()
+num_cache(::Union{Val{4},StaticInt{4}}) = num_l4cache()
+const BASELINE_CORES = Int(num_cores()) * ((Sys.ARCH === :aarch64) && Sys.isapple() ? 2 : 1)
+
+__init__() = _redefine()
+
