5b9b3c2196b50fc9a2cbf7155347b2b4e80082eb
chriselrod <elrodc@gmail.com>
2023-09-13 20:48:25 -0400

    Asahi

--- src/generic_topology.jl
+++ src/generic_topology.jl
@@ -18,7 +18,7 @@ else
 end
 num_l4cache() = static(0)
 
-if Sys.ARCH === :aarch64 && Sys.isapple()
+if Sys.ARCH === :aarch64 && (Sys.isapple() || occursin("apple", Sys.CPU_NAME::String))
   cache_size(::Union{Val{1},StaticInt{1}}) = StaticInt{131072}()
 else
   cache_size(::Union{Val{1},StaticInt{1}}) = StaticInt{32768}()
@@ -27,7 +27,7 @@ cache_associativity(::Union{Val{1},StaticInt{1}}) = StaticInt{0}()
 cache_type(::Union{Val{1},StaticInt{1}}) = Val{:Data}()
 cache_inclusive(::Union{Val{1},StaticInt{1}}) = False()
 
-if Sys.ARCH === :aarch64 && Sys.isapple()
+if Sys.ARCH === :aarch64 && (Sys.isapple() || occursin("apple", Sys.CPU_NAME::String))
   cache_size(::Union{Val{2},StaticInt{2}}) = StaticInt{3145728}()
 else
   cache_size(::Union{Val{2},StaticInt{2}}) = StaticInt{65536}()
@@ -35,14 +35,17 @@ end
 cache_associativity(::Union{Val{2},StaticInt{2}}) = StaticInt{0}()
 cache_type(::Union{Val{2},StaticInt{2}}) = Val{:Unified}()
 cache_inclusive(_) = False()
-@static if Sys.isapple() && Sys.ARCH === :aarch64
+@static if Sys.ARCH === :aarch64 && (Sys.isapple() || occursin("apple", Sys.CPU_NAME::String))
   cache_linesize(_) = StaticInt{128}() # assume...
 else
   cache_linesize(_) = StaticInt{64}() # assume...
 end
 cache_size(_) = StaticInt{0}()
 
-cache_type(::Union{Val{3},StaticInt{3}}) = Val{:Unified}()
-cache_size(::Union{Val{3},StaticInt{3}}) = StaticInt{1441792}()
+@static if Sys.ARCH === :aarch64 && (Sys.isapple() || occursin("apple", Sys.CPU_NAME::String))
+else
+  cache_type(::Union{Val{3},StaticInt{3}}) = Val{:Unified}()
+  cache_size(::Union{Val{3},StaticInt{3}}) = StaticInt{1441792}()
+end
 
 _extra_init() = nothing
